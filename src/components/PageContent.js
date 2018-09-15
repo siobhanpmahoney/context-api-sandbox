d05f3f095243b2d6586cca6ce18cf96d307f5728
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-09-14 23:47:28 -0400

    example with class component

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -2,8 +2,24 @@ import React from 'react'
 import {settings, DisplayContext} from '../context/DisplayContext'
 
 
-export function PageContent() {
-  // render() {
+// export function PageContent() {
+//   // render() {
+//   return (
+//     <DisplayContext.Consumer>
+//       {({setting, toggleSetting}) => (
+//         <div style={{fontFamily: setting.fontFamily}}>
+//           <h1>Learning Context Api </h1>
+//           <h2> Part 1: The Basics </h2>
+//         </div>
+//       )}
+//     </DisplayContext.Consumer>
+//   )
+//   // }
+// }
+
+
+class PageContent extends React.Component {
+  render() {
   return (
     <DisplayContext.Consumer>
       {({setting, toggleSetting}) => (
@@ -14,7 +30,8 @@ export function PageContent() {
       )}
     </DisplayContext.Consumer>
   )
-  // }
+  }
 }
 
-// export default PageContent
+
+export default PageContent
